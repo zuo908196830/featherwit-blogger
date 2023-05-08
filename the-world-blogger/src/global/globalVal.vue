beb59862a375977fb34c38656ea655aae9185835
CucerateTR <908196830@qq.com>
2023-05-08 18:15:47 +0800

    fix: 小bug

---
+++ the-world-blogger/src/global/globalVal.vue
@@ -0,0 +1,15 @@
+<script>
+    var blogs = []
+    var searchName = ''
+    var total = 0
+    var limit = 0
+    var page = 0
+
+    export default {
+        blogs,
+        searchName,
+        total,
+        limit,
+        page
+    }
+</script>
